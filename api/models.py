1e9bc8e2c0dc6c248382bac15ff9b83559d3ca5c
Erik-Serbicki <77897214+Erik-Serbicki@users.noreply.github.com>
2024-05-22 13:17:41 -0700

    created model for room

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_length=6, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
