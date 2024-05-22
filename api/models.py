8b617b990baa5fde04cbf5aac0e658cd88a832c0
Erik-Serbicki <77897214+Erik-Serbicki@users.noreply.github.com>
2024-05-22 13:22:42 -0700

    added code generation to Room model

--- api/models.py
+++ api/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+import random
+import string
 
-# Create your models here.
+# Length of room code
+CODE_LENGTH = 6
+
+# Generate a unique code
+def generate_unique_code():
+    while True:
+        # Create a random string of letters
+        code = ''.join(random.choices(string.ascii_uppercase, k=CODE_LENGTH))
+        
+        # If there is already a room with that code, generate a new one
+        if Room.objects.filter(code=code).count() == 0:
+            return code
+    
+    
+
+# Room model
 class Room(models.Model):
-    code = models.CharField(max_length=6, default="", unique=True)
+    code = models.CharField(max_length=CODE_LENGTH, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
